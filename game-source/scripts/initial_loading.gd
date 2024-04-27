695d57a1f167572079a41c916490f54adcba1898
peachey2k2 <ogzpolat@gmail.com>
2024-04-27 20:34:46 +0300

    add a reconnect button to initial loading screen in case the server is dead

--- game-source/scripts/initial_loading.gd
+++ game-source/scripts/initial_loading.gd
@@ -1,14 +1,25 @@
 extends CanvasLayer
 
-@onready var spinner: TextureRect = $Panel/Spinner
+@onready var spinner: TextureRect = $Connecting/Spinner
+@onready var connecting: Panel = $Connecting
+@onready var no_connecton: Panel = $"No Connecton"
 
 const MAIN_MENU = preload("res://scenes/main_menu.tscn")
 
 func _ready() -> void:
 	Global.initialized.connect(Callable(self, "_on_initialized"))
+	GameSocket.connection_failed.connect(Callable(self, "_on_connection_failed"))
 
 func _physics_process(_delta: float) -> void:
 	spinner.rotation -= 0.1
 
 func _on_initialized():
 	get_tree().change_scene_to_packed(MAIN_MENU)
+
+func _on_connection_failed():
+	connecting.hide()
+	no_connecton.show()
+	await $"No Connecton/Retry".pressed
+	connecting.show()
+	no_connecton.hide()
+	GameSocket.reconnect.emit()
