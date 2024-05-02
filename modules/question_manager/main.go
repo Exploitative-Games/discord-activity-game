df18ba8b5bcd8c9fefa53e8fbae87e934eca382c
Manti <67705577+mantikafasi@users.noreply.github.com>
2024-05-02 13:32:30 +0300

    trim spaces

--- modules/question_manager/main.go
+++ modules/question_manager/main.go
@@ -19,5 +19,11 @@ func GetCategoryWithID(id int) (category database.Category, err error) {
 func GetRandomQuestionWithCategoryID(categoryID int) (question database.Question, err error) {
 	err = database.DB.NewSelect().Model(&question).Where("category_id = ?", categoryID).OrderExpr("RANDOM()").Limit(1).Scan(context.Background(), &question)
 	question.PossibleAnswers = strings.Split(strings.ToLower(question.PossibleAnswersString), ",")
+	
+	// remove spaces
+	for i := range question.PossibleAnswers {
+		question.PossibleAnswers[i] = strings.TrimSpace(question.PossibleAnswers[i])
+	}
+	
 	return
 }
